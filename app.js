296ee54fbb77464b6e655b31b141e583c7a73596
Kashif1Naqvi <mrkashif029@gmail.com>
2020-07-13 12:38:48 +0500

    store data to localestorage

--- app.js
+++ app.js
@@ -4,25 +4,28 @@ const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo')
 
 // Event Listerners
-
+document.addEventListener('DOMContentLoaded',getTodo)
 todoButton.addEventListener('click',addTodo);
 filterOption.addEventListener('click',filterTodo);
 
 // Function     
 function addTodo(event){
     event.preventDefault()
-    
+
     todoList.addEventListener('click',deletecheck)
     
 
     const todoDiv = document.createElement('div')
     todoDiv.classList.add('todo')
 
+    // ADD TODO TO LOCALCLESTORAGE
+    saveLocaleTodo(todoInput.value)    
+
     // create li
 
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value
-    newTodo.classList.add('tofo-item')
+    newTodo.classList.add('todo-item')
     todoDiv.appendChild(newTodo)
 
     // Check mark button
@@ -44,7 +47,6 @@ function addTodo(event){
     // append todo-list
 
     todoList.appendChild(todoDiv)
-
     // clear todo input value
     todoInput.value = ''
 }
@@ -99,5 +101,56 @@ function filterTodo(e){
                 break;
         }
     });
+}
+
+
+
+function saveLocaleTodo(todo){
+    // Check --- Hey Do I already have in there?
+    let todos;
+    
+    if(localStorage.getItem("todos") === null ){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+
+    todos.push(todo)
+    localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+
+function getTodo(){
+    let todos;
+    if(localStorage.getItem('todos') === null ){
+        todos = []
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+
+    todos.forEach((todo)=>{
+        todoList.addEventListener('click',deletecheck)
+
+        let todoDiv = document.createElement('div')
+        todoDiv.classList.add('todo')
+
+        let newTodo = document.createElement('li')
+        newTodo.innerText = todo
+        newTodo.classList.add('todo-item')
+        todoDiv.appendChild(newTodo)
+        
+
+        let chechBtn = document.createElement('button')
+        chechBtn.classList.add('complete-btn')
+        chechBtn.innerHTML = '<i class="fas fa-check" ></i>'
+        todoDiv.appendChild(chechBtn)        
+
+        let trashbtn = document.createElement('button')
+        trashbtn.classList.add("trash-btn")
+        trashbtn.innerHTML = '<i class="fas fa-trash" ></i>'
+        todoDiv.appendChild(trashbtn)
+
+        todoList.append(todoDiv)        
+    })
 }
 
