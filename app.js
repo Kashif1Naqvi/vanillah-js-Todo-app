708bbadc112967aef219b02de6dce3353e7f5400
Kashif1Naqvi <mrkashif029@gmail.com>
2020-07-13 10:53:19 +0500

    filter todo completed all uncompleted

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-
+const filterOption = document.querySelector('.filter-todo')
 
 // Event Listerners
 
 todoButton.addEventListener('click',addTodo);
-
+filterOption.addEventListener('click',filterTodo);
 
 // Function     
 function addTodo(event){
@@ -57,8 +57,47 @@ function deletecheck(e){
    // DELETE TODO
    if(item.classList[0]==='trash-btn'){
        const todo = item.parentElement
-       todo.remove()
-
+       // ADD ANIMATION
+       todo.classList.add("fall")
+       todo.addEventListener("transitionend",function(){
+        todo.remove()
+    })
+       
    }
 
+   // Check mark 
+
+   if(item.classList[0]==='complete-btn'){
+       const todo = item.parentElement
+       todo.classList.toggle('completed')
+   }
+
+
 }
+
+
+function filterTodo(e){
+    const todo = todoList.childNodes;
+    todo.forEach(todo => {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = 'flex'
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                     todo.style.display = 'flex'
+                }else{
+                    todo.style.display = 'none'
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = 'flex'
+                }else{
+                    todo.style.display = 'none'
+                }
+                break;
+        }
+    });
+}
+
